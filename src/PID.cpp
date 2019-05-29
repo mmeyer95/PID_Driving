9aa6626204a23057201e285ca1d9372a6be0c6bb
mmeyer95 <meredith_meyer@berkeley.edu>
2019-05-29 02:34:53 +0000

    Added inits of error and constants.

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,13 +12,27 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  //Change these values later
+  Kp=1;
+  Ki=1;
+  Kd=1;
+  
+  //start with zero error
+  i_error = 0;
+  d_error = 0;
 
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+
+  //UPDATE PID values
+  Kp = cte*Kp; //proportional constant... what should the constant be?
+  
+  d_error = cte - d_error; //constant equal to the change in error
+  Kd = Kd*d_error;
+  
+  i_error += cte; //integral error keeps track of total error
+  Ki = i_error * Ki;
 
 }
 
